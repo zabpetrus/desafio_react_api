3de8def5af29b5668a8f362c4aba365a8b096c93
belsouza <belsanpesou@yahoo.com.br>
2024-05-24 08:40:09 -0300

    Consertando o erro de submodulo

---
+++ frontend_react/src/components/AppFooter/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+}
+
+const AppFooter = (props: Props) => {
+    <footer className='footer'>
+        <div className='row'>
+            @Todos os direitos reservados
+        </div>
+    </footer>
+};
+
+export default AppFooter;
---
+++ frontend_react/src/components/AppHeader/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+interface Props {
+}
+
+const AppHeader = (props: Props) => {
+    return null;
+};
+
+export default AppHeader;
